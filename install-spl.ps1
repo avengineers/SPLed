05f5171fbe14e810516133915e9d90e19797640c
Maxiniuc, Alexandru (SD-RM) <Alexandru.Maxiniuc@marquardt.com>
2022-08-24 15:47:10 +0200

    read spl core installer from the dependencies.json

--- install-spl.ps1
+++ install-spl.ps1
@@ -1,8 +1,17 @@
 $dependency_json_content = Get-Content -Raw -Path $PSScriptRoot\dependencies.json | ConvertFrom-Json
-if ($dependency_json_content.mandatory.spl_core_version) {
-    $spl_version = $dependency_json_content.mandatory.spl_core_version
+if ($dependency_json_content.mandatory.spl_core.version) {
+    $spl_version = $dependency_json_content.mandatory.spl_core.version
 } else {
-    Write-Error "SPL core version not defined in the dependencies.json. Please define 'spl-core-version' in the 'mandatory' dependencies."
+    Write-Error "SPL core version not defined in the dependencies.json. Please define 'mandatory.spl-core.version' value."
     exit 1
 }
-Invoke-Expression "& { $(Invoke-RestMethod https://raw.githubusercontent.com/avengineers/SPL/develop/install.ps1) } $spl_version"
+if ($dependency_json_content.mandatory.spl_core.installer) {
+    $spl_installer = $dependency_json_content.mandatory.spl_core.installer
+} else {
+    Write-Error "SPL core installer not defined in the dependencies.json. Please define 'mandatory.spl-core.installer' value."
+    exit 1
+}
+if (-not $dependency_json_content.mandatory.spl_core.url) {
+    Write-Warning "SPL core repository url not defined in the dependencies.json. The SPL Github repository will be used by default. Please define 'mandatory.spl-core.url' in case you want to use an SPL mirror repository."
+}
+Invoke-Expression "& { $(Invoke-RestMethod $spl_installer) } $spl_version"
