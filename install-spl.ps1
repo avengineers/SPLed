9b940bde1de1f69be1e07f04eb06a01f0b96b423
Eggert, Matthias (SD-RM) <Matthias.Eggert@marquardt.com>
2022-08-29 08:53:57 +0200

    pimp installation of spl-core with proper version dependency handling and by calling install-spl-ps1 directly

--- install-spl.ps1
+++ install-spl.ps1
@@ -11,7 +11,12 @@ if ($dependency_json_content.mandatory.spl_core.installer) {
     Write-Error "SPL core installer not defined in the dependencies.json. Please define 'mandatory.spl-core.installer' value."
     exit 1
 }
-if (-not $dependency_json_content.mandatory.spl_core.url) {
+if ($dependency_json_content.mandatory.spl_core.url) {
+    $additional_args = $dependency_json_content.mandatory.spl_core.url
+} else {
     Write-Warning "SPL core repository url not defined in the dependencies.json. The SPL Github repository will be used by default. Please define 'mandatory.spl-core.url' in case you want to use an SPL mirror repository."
 }
-Invoke-Expression "& { $(Invoke-RestMethod $spl_installer) } $spl_version"
+
+Push-Location $PSScriptRoot
+Invoke-Expression "& { $(Invoke-RestMethod $spl_installer) } $spl_version $additional_args -skipInstall"
+Pop-Location
