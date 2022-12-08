bfc581258f1629523e3c74e93964f0a65cc030e1
Guenther, Karsten (SD-RM) <Karsten.Guenther@marquardt.com>
2022-12-08 12:24:22 +0100

    Add small extension example

--- build.bat
+++ build.bat
@@ -1,12 +1,10 @@
 @echo off
 
 pushd %~dp0
-powershell -Command Set-ExecutionPolicy Bypass -scope CurrentUser -force || exit /b 1
 IF DEFINED SPLCORE_PATH (
-    echo WARNING: Fixed SPL core path defined. Skip updating SPL core.
-    powershell -File %SPLCORE_PATH%\powershell\spl.ps1 %* || exit /b 1
+    echo WARNING: Fixed SPL core path defined.
+    powershell -ExecutionPolicy Bypass -File %SPLCORE_PATH%\powershell\spl.ps1 %* || exit /b 1
 ) else (
-    if not exist build\spl-core\ powershell -File install-spl.ps1 || exit /b 1
-    powershell -File build\spl-core\powershell\spl.ps1 %* || exit /b 1
+    powershell -ExecutionPolicy Bypass -File modules\spl\powershell\spl.ps1 %* || exit /b 1
 )
 popd
