2ec6d57035c1da1a238a18b60e17d3e671fb956e
Guenther, Karsten (SD-RM) <Karsten.Guenther@marquardt.com>
2022-12-21 11:18:16 +0100

    Back to the roots

--- build.ps1
+++ build.ps1
@@ -1,16 +1,3 @@
-$ErrorActionPreference = "Stop"
-
 Push-Location $PSScriptRoot
-
-Write-Output "Running in ${pwd}"
-
-. .\modules\spl\powershell\include.ps1
-
-Invoke-CommandLine -CommandLine "powershell .\modules\spl\powershell\spl-install.ps1 $args"
-ReloadEnvVars
-
-if($args -contains "-build" -or $args -contains "-import") {
-    Invoke-CommandLine -CommandLine "python -m pipenv run powershell .\modules\spl\powershell\spl.ps1 $args"
-}
-
+Invoke-Expression ".\modules\spl\powershell\spl.ps1 $args"
 Pop-Location
