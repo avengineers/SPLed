f6aa456a629d8771f4b465a339df7cd3183b16cd
Eggert, Matthias (SD-RM) <Matthias.Eggert@marquardt.com>
2023-01-05 09:37:25 +0100

    add gitignores to porperly handle modules

--- git-settings.ps1
+++ git-settings.ps1
@@ -15,6 +15,18 @@ if (-not (Test-Path -Path $markerPath)) {
 
     Write-Host "Done."
     New-Item -Path "$markerPath" -ItemType File -Force
+
+    $gitignorecontent = @"
+# Ignore everything
+*
+# But descend into directories
+!*/
+# !*/ by itself will only allow descent into the full directory tree; it won’t actually allow anything into the repo; so include .gitignore of subdirectories
+# it is important! that <subdirectory>/.gitignore files contain "!*" as first line to allow adding new files in subdirectories
+!*/**/.gitignore
+"@
+
+ Set-Content "${PSScriptRoot}/modules/.gitignore" $gitignorecontent
 }
 else {
     Write-Host -ForegroundColor Black -BackgroundColor Yellow "Skipping git settings setup; delete $markerPath to run it again."
---
+++ modules/not-a-submodule-extension/index.cmake
@@ -0,0 +1,1 @@
+message("Bring it on!")
