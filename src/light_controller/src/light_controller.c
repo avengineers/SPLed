a6e81a6305e8f8cd91eb0d822a859ff7cfced9f9
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2023-09-29 08:52:16 +0200

    Played a bit with GTest auf auto formatter

--- src/light_controller/src/light_controller.c
+++ src/light_controller/src/light_controller.c
@@ -6,10 +6,10 @@
 #include "light_controller.h"
 #include "rte.h"
 
-/**
- * @enum LightState
- * @brief Represents the states of the light.
- */
+ /**
+  * @enum LightState
+  * @brief Represents the states of the light.
+  */
 typedef enum {
     LIGHT_OFF,  /**< Represents a state where the light is turned off. */
     LIGHT_ON    /**< Represents a state where the light is turned on with a specific color. */
@@ -20,7 +20,7 @@ static LightState currentLightState = LIGHT_OFF;  /**< Current state of the ligh
 /**
  * @brief Controls the light behavior based on the power state.
  *
- * Uses a state machine to determine the light's color. The light color is only updated 
+ * Uses a state machine to determine the light's color. The light color is only updated
  * when there's a change in the power state to ensure no redundant updates.
  */
 void lightController(void) {
@@ -28,34 +28,34 @@ void lightController(void) {
     PowerState powerState = RteGetPowerState();
 
     switch (currentLightState) {
-        case LIGHT_OFF:
-            if (powerState != POWER_STATE_OFF) {
-                // Transition action: turn the light on with specific color
-                RGBColor color = {
-                    .red = 0,
-                    .green = 128,
-                    .blue = 55
-                };
-                RteSetLightValue(color);
+    case LIGHT_OFF:
+        if (powerState != POWER_STATE_OFF) {
+            // Transition action: turn the light on with specific color
+            RGBColor color = {
+                .red = 0,
+                .green = 128,
+                .blue = 55
+            };
+            RteSetLightValue(color);
 
-                // Transition to LIGHT_ON state
-                currentLightState = LIGHT_ON;
-            }
-            break;
+            // Transition to LIGHT_ON state
+            currentLightState = LIGHT_ON;
+        }
+        break;
 
-        case LIGHT_ON:
-            if (powerState == POWER_STATE_OFF) {
-                // Transition action: turn the light off
-                RGBColor color = {
-                    .red = 0,
-                    .green = 0,
-                    .blue = 0
-                };
-                RteSetLightValue(color);
+    case LIGHT_ON:
+        if (powerState == POWER_STATE_OFF) {
+            // Transition action: turn the light off
+            RGBColor color = {
+                .red = 0,
+                .green = 0,
+                .blue = 0
+            };
+            RteSetLightValue(color);
 
-                // Transition to LIGHT_OFF state
-                currentLightState = LIGHT_OFF;
-            }
-            break;
+            // Transition to LIGHT_OFF state
+            currentLightState = LIGHT_OFF;
+        }
+        break;
     }
 }
--- src/light_controller/test/test_light_controller.cc
+++ src/light_controller/test/test_light_controller.cc
@@ -1,5 +1,5 @@
 /**
- * @file 
+ * @file
  */
 #include <gtest/gtest.h>
 using namespace testing;
@@ -12,9 +12,9 @@ extern "C" {
 #include "mockup_src_light_controller.h" // Assuming you have mock sources for the light controller.
 
 bool areRGBColorsEqual(const RGBColor* color1, const RGBColor* color2) {
-    return color1->red == color2->red && 
-           color1->green == color2->green && 
-           color1->blue == color2->blue;
+    return color1->red == color2->red &&
+        color1->green == color2->green &&
+        color1->blue == color2->blue;
 }
 
 MATCHER_P(RGBColorEq, expected, "") {
@@ -42,24 +42,20 @@ TEST(light_controller, test_light_on_and_off)
     lightController();
 
     // Power turns ON, so the light should turn ON.
-    EXPECT_CALL(mymock, RteGetPowerState()).WillOnce(Return(POWER_STATE_ON));
-    RGBColor onColor = {.red = 0, .green = 128, .blue = 55};
-    EXPECT_CALL(mymock, RteSetLightValue(RGBColorEq(onColor))).Times(1); // Expect that the light value changes to ON color.
-    lightController();
-
-    // Power remains ON, but the light should not change (no redundant updates).
-    EXPECT_CALL(mymock, RteGetPowerState()).WillOnce(Return(POWER_STATE_ON));
-    EXPECT_CALL(mymock, RteSetLightValue(_)).Times(0); // Expect that the light value doesn't change.
-    lightController();
+    EXPECT_CALL(mymock, RteGetPowerState()).WillRepeatedly(Return(POWER_STATE_ON));
+    RGBColor onColor = { .red = 0, .green = 128, .blue = 55 };
+    // Expect that the light value changes to ON color (only once, no redundant updates).
+    EXPECT_CALL(mymock, RteSetLightValue(RGBColorEq(onColor))).Times(1);
+    for (int i = 0; i < 10; i++) {
+        lightController();
+    }
 
     // Power turns OFF, so the light should turn OFF.
-    EXPECT_CALL(mymock, RteGetPowerState()).WillOnce(Return(POWER_STATE_OFF));
-    RGBColor offColor = {.red = 0, .green = 0, .blue = 0};
-    EXPECT_CALL(mymock, RteSetLightValue(RGBColorEq(offColor))).Times(1); // Expect that the light value changes to OFF color.
-    lightController();
-
-    // Power remains OFF, but the light should not change (no redundant updates).
-    EXPECT_CALL(mymock, RteGetPowerState()).WillOnce(Return(POWER_STATE_OFF));
-    EXPECT_CALL(mymock, RteSetLightValue(_)).Times(0); // Expect that the light value doesn't change.
-    lightController();
+    EXPECT_CALL(mymock, RteGetPowerState()).WillRepeatedly(Return(POWER_STATE_OFF));
+    RGBColor offColor = { .red = 0, .green = 0, .blue = 0 };
+    // Expect that the light value changes to OFF color (only once, no redundant updates).
+    EXPECT_CALL(mymock, RteSetLightValue(RGBColorEq(offColor))).Times(1);
+    for (int i = 0; i < 10; i++) {
+        lightController();
+    }
 }
