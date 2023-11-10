e6480763b3014726cefa9678560774a0f1c2cc18
Kobagapu, Akshit (RDS-RM) <Akshit.Kobagapu@marquardt.com>
2023-11-10 07:15:33 +0100

    fix: create artifacts archive

--- test/CustB/Sleep/test_CustB__Sleep.py
+++ test/CustB/Sleep/test_CustB__Sleep.py
@@ -1,15 +1,13 @@
+import json
 import os.path
-from utils import create_artifacts_collection, spl_build
+from pathlib import Path
+from utils import ArtifactsCollection, spl_build
 
 
 class Test_CustB__Sleep:
     @classmethod
     def setup_class(cls):
         cls.variant = "CustB/Sleep"
-        cls.build_dir = "build/CustB/Sleep"
-        cls.artifacts_collection = create_artifacts_collection(
-            cls.variant, cls.build_dir
-        )
 
     def test_unit_tests(self):
         """Unit tests execution shall be successful."""
@@ -23,16 +21,31 @@ class Test_CustB__Sleep:
         assert 0 == spl_build(self.variant, "prod", "all")
 
         """executable shall exist and collected for the bom."""
-        self.artifacts_collection.collect(f"build/{self.variant}/prod/spled.exe")
-
-        self.artifacts_collection.create_bom()
+        artifacts_collection = ArtifactsCollection(self.variant)
+        artifacts_collection.collect(f"build/{self.variant}/*/*.exe")
 
         """executable shall exist and collected for the BOM."""
-        assert os.path.isfile(f"build/{self.variant}/prod/spled.exe")
+        assert len(artifacts_collection.artifacts) > 0
 
         """BOM shall be created"""
-        bom_path = os.path.join(self.build_dir, "bom.json")
-        assert os.path.isfile(bom_path)
+        bom_json_path = Path(f"build/{self.variant}/prod/bom.json")
+        artifacts_collection.create_bom(bom_json_path)
+        assert bom_json_path.is_file()
+
+        """BOM shall contain the expected artifacts"""
+        expected_artifacts = [
+            os.path.basename(artifact) for artifact in artifacts_collection.artifacts
+        ]
+        with open(bom_json_path, "r") as bom_file:
+            bom = json.load(bom_file)
+            assert bom["variant"] == self.variant
+            assert set(bom["artifacts"]) == set(expected_artifacts)
+
+        """variant's artifacts zip file shall be created"""
+        variant_path = self.variant.replace("/", "_")
+        artifacts_zip_path = Path(f"build/{self.variant}/prod/{variant_path}.zip")
+        artifacts_collection.create_artifacts_zip(artifacts_zip_path)
+        assert artifacts_zip_path.is_file()
 
     def test_reports(self):
         """Reports generation shall be successful."""
