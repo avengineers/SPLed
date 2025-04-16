6df9bbdceae7697c995aeaee9646aa3de6118804
Karsten A. M. Guenther <karsten.guenther@kamg.de>
2025-04-16 21:49:21 +0200

    feat: find compiler's root path in toolchains, cmake config in VSCode

--- tools/toolchains/clang/toolchain.cmake
+++ tools/toolchains/clang/toolchain.cmake
@@ -2,5 +2,13 @@ set(CMAKE_C_COMPILER clang CACHE STRING "C Compiler")
 set(CMAKE_CXX_COMPILER clang++ CACHE STRING "CXX Compiler")
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER} CACHE STRING "ASM Compiler")
 
-# TODO: clarify why llvm-cov produces invalid gcov files (contain blank lines), releated GCOVR issue: https://github.com/gcovr/gcovr/issues/331
-set(GCOVR_ADDITIONAL_OPTIONS --gcov-executable \"llvm-cov gcov\" --gcov-ignore-parse-errors --html-title \"Code Coverage Report \(tool suite: LLVM Clang\)\")
+find_program(CMAKE_C_COMPILER_PATH_EXE ${CMAKE_C_COMPILER} REQUIRED)
+get_filename_component(CMAKE_C_COMPILER_PATH_BIN ${CMAKE_C_COMPILER_PATH_EXE} DIRECTORY)
+get_filename_component(CMAKE_C_COMPILER_PATH_ROOT ${CMAKE_C_COMPILER_PATH_BIN} DIRECTORY)
+message(STATUS "CMAKE_C_COMPILER_PATH_ROOT: ${CMAKE_C_COMPILER_PATH_ROOT}")
+
+include_directories(BEFORE
+    ${CMAKE_C_COMPILER_PATH_ROOT}/include
+    ${CMAKE_C_COMPILER_PATH_ROOT}/lib/clang/19/include
+    ${CMAKE_C_COMPILER_PATH_ROOT}/x86_64-w64-mingw32/include
+)
