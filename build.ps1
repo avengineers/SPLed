9062ed98b5f877a097bfc2242db4d200295398a2
Kobagapu, Akshit (RDDC-RM) <Akshit.Kobagapu@marquardt.com>
2026-01-06 11:15:45 +0100

    feat: update bootstrap version to v1.18.0

--- build.ps1
+++ build.ps1
@@ -269,7 +269,7 @@ function Get-User-Menu-Selection {
 
 function Invoke-Bootstrap {
     # Download bootstrap scripts from external repository
-    Invoke-RestMethod -Uri https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.17.2/install.ps1 | Invoke-Expression
+    Invoke-RestMethod -Uri https://raw.githubusercontent.com/avengineers/bootstrap-installer/v1.18.0/install.ps1 | Invoke-Expression
     # Execute bootstrap script
     . .\.bootstrap\bootstrap.ps1
 }
